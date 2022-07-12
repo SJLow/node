5a4506aa233bbe540f0bf5a57201b91b56dc4209
LOW SHI JER <lowshijer@LOWs-MacBook-Air.local>
2022-07-12 15:52:04 +0800

    fullcommit

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const app = express();
-const port = process.env.port || 3000;
+// const express = require('express');
+// const app = express();
+// const port = process.env.port || 3000;
 
-app.listen(port, () => {
-    console.log("Wazzapppp");
+// app.listen(3000, () => {
+//     console.log("Wazzapppp");
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+// app.get('/', (req, res) => {
+//     res.send("Welcome to the home page baby");
+// });
+
+
+var app = require("express")();
+
+app.get("/", function (req, res) {
+    console.log(req.socket.remoteAddress);
+    console.log(req.ip);
+    res.send("your IP is: " + req.ip);
 });
-
-app.get('/', (req, res) => {
-    res.send("Welcome to the home page baby");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("server running on port: " + PORT);
 });
