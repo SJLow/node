0a6e0d47422a1d399da647005cc311cc296006cc
LOW SHI JER <lowshijer@LOWs-MacBook-Air.local>
2022-07-12 14:28:52 +0800

    variable_port

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express();
+const port = process.env.port || 3000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Wazzapppp");
 });
 
